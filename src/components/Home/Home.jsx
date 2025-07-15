d4e1c2b45fe32016279b58487e9cd9f15e060213
unknown <mebannongrum333@gmail.com>
2025-07-15 07:16:57 +0530

    refractor failed test cases- modal click outside

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import styles from './Home.module.css'
 import Modal from '../Modal/Modal';
 import UserForm from '../Form/UserForm';
 import ReactModal from 'react-modal';
-ReactModal.setAppElement('#root');
+// ReactModal.setAppElement('#root');
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
