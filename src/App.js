40585c0c57c1a25d9ae84cb27bf8bf26ca4bc8fe
unknown <mebannongrum333@gmail.com>
2025-07-15 07:36:01 +0530

    close modal test failure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 // import UserForm from "./components/Form/UserForm";
 import Home from "./components/Home/Home";
+import ModalWrapper from "./components/Modal/Modal";
+import UserForm from "./components/Form/UserForm";
+import ReactModal from 'react-modal';
+import { useState } from "react";
+// ReactModal.setAppElement('#root');
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Home />
+    <>
+      <Home setIsOpen={setIsOpen}/>
+    <ModalWrapper isOpen={isOpen} setIsOpen={setIsOpen}>
+      <UserForm setIsOpen={setIsOpen}/>
+    </ModalWrapper>
+    </>    
   );
 }
 
